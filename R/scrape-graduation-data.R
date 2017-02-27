dc3b448fa29ade4aa0f37ad978383f38bece1293
Karan Tibrewal <karantib@mit.edu>
2017-02-27 17:06:49 -0500

    deleted download functionality + added info for 2000-01 graduates

---
+++ R/scrape-graduation-data.R
@@ -0,0 +1,6 @@
+#' @title Summa Cum Laude Scraper
+#' @description Scrapes data for Summa Cum Laude graduates for given year
+#' @param year year to scrape data for
+#' @return A list of Summa Cum Laude graduates w/ honors information
+scrape_suma_cum_laude <- function(year){
+}
