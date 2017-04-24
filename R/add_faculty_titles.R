886875f8b0a904522e8c73e4218542588f51c6ba
Karan Tibrewal <karantib@mit.edu>
2017-04-24 14:32:43 -0400

    fixed faculty$rank, and added column faculty$status

--- R/add_faculty_titles.R
+++ R/add_faculty_titles.R
@@ -10,7 +10,9 @@
 #'
 #' @format \describe{
 #'  \item{title}{Faculty's title}
-#'  \item{rank}{Faculty's rank}
+#'  \item{rank}{Faculty's rank; one of "Librarian", "Instructor", "Fellow", "Lecturer", "Visiting Professor",
+#'  "Assistant Professor", "Associate Professor", "Professor"}
+#'  \item{status}{Faculty's status; as one of "Visiting", "Part-time", Tenure-track", or "Tenured"}
 #'  }
 #'
 #' @importFrom dplyr %>%
@@ -27,18 +29,28 @@ add_faculty_titles <- function(x){
   ## in the raw text
   x$title <- stringr::str_split(x$raw.text, "#", simplify = TRUE)[ ,2] %>% stringr::str_trim()
 
-  ## Now add rank as one of "Librarian", "Instructor", "Fellow", "Lecturer", "Visiting Professor",
+  ## Now add rank as one of "Librarian", "Instructor", "Fellow", "Lecturer", "Artist-in-Residence","Visiting Professor",
   ## "Assistant Professor", "Associate Professor", "Professor"
-  x$rank <- ""
-  x$rank[which(stringr::str_detect(x$title, "Fellow"))] <- "Fellow"
-  x$rank[which(stringr::str_detect(x$title, "Librarian"))] <- "Librarian"
-  x$rank[which(stringr::str_detect(x$title, "Instructor"))] <- "Instructor"
-  x$rank[which(stringr::str_detect(x$title, "Lecturer") & x$rank == "")] <- "Lecturer"
-  x$rank[which(stringr::str_detect(x$title, "Visiting") & x$rank == "")] <- "Visiting Professor"
-  x$rank[which(stringr::str_detect(x$title, "Assistant Professor") & x$rank == "")] <- "Assistant Professor"
-  x$rank[which(stringr::str_detect(x$title, "Associate Professor") & x$rank == "")] <- "Associate Professor"
-  x$rank[which(stringr::str_detect(x$title, "Professor") & x$rank == "")] <- "Professor"
+  x$rank <- NA
+  x$rank[which(stringr::str_detect(x$title, "Assistant Professor"))] <- "Assistant Professor"
+  x$rank[which(stringr::str_detect(x$title, "Associate Professor") & is.na(x$rank))] <- "Associate Professor"
+  x$rank[which(stringr::str_detect(x$title, "Professor") & is.na(x$rank))] <- "Professor"
+  x$rank[which(stringr::str_detect(x$title, "Fellow") & is.na(x$rank))] <- "Fellow"
+  x$rank[which(stringr::str_detect(x$title, "Librarian") & is.na(x$rank))] <- "Librarian"
+  x$rank[which(stringr::str_detect(x$title, "Instructor") & is.na(x$rank))] <- "Instructor"
+  x$rank[which(stringr::str_detect(x$title, "Lecturer") & is.na(x$rank))] <- "Lecturer"
+  x$rank[which(stringr::str_detect(x$title, "Artist.+in.+Residence") & is.na(x$rank))] <- "Artist-in-Residence"
 
+
+  ## Now add status as one of "Visiting", "Part-time", Tenure-track", or "Tenured"
+  ## The course catalogs do not give information about "tenure-track" or "tenured".
+  ## We assume that all "associate" or "full" professors are "tenured".
+  ## All "assisstant" professors are "tenure-track"
+  x$status <- NA
+  x$status[which(stringr::str_detect(x$title, "Part-time") | stringr::str_detect(x$title, "Part-Time"))] <- "Part-time"
+  x$status[which(x$rank == "Associate Professor" | x$rank == "Professor")] <- "Tenured"
+  x$status[which(x$rank == "Assistant Professor")] <- "Tenure-track"
+  x$status[which(stringr::str_detect(x$title, "Visiting"))] <- "Visiting"
 
   ## Some error checking
   stopifnot(all(x$title != ""))
