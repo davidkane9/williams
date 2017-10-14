f02355e42ad07f1cd5fda43ecf8b6f4d3216bf64
davidkane9 <dave.kane@gmail.com>
2017-10-14 07:46:55 -0400

    clean up documentation of two included data frames

--- R/graduates.R
+++ R/graduates.R
@@ -1,23 +1,25 @@
 #' Williams College Graduates
 #'
-#' A dataset containing graduation information for 8,893 ephs (classes of 2001 to 2016), extracted from the
-#' Williams College course catalog. graduates_details is a similar data frame, with the same
-#' number of rows and all the same variables. It also includes a dozen or so other variables.
+#' A tibble containing graduation information for 8,893 ephs (classes of 2001 to
+#' 2016), extracted from the Williams College course catalog. 
 #'
 #' @source \url{https://catalog.williams.edu/archive/}
 #'
-#' @format A data frame with 8,893 rows and 10 variables:
+#' @format A tibble with 8,893 rows and 10 variables:
 #' \describe{
-#'     \item{year}{numeric, year of graduation}
-#'     \item{first.name}{character, first name of graduate}
-#'     \item{last.name}{character, last name of graduate}
-#'     \item{latin.honors}{character, Latin Honors recieved by graduate (if any)}
-#'     \item{Phi.Beta.Kappa}{lgical, indicates Phi Beta Kappa membership}
-#'     \item{Sigma.Xi}{lgical, indicates Sigma Xi membership}
-#'     \item{honor}{character, first department honor awarded, if any.}
-#'     \item{major}{character, academic field in which graduate won primary honor.}
-#'     \item{gender}{character, based on \code{first.name}, using the gender package.}
-#'     \item{race}{character, based on \code{last.name}, using the wru package.}
+#'     \item{year}{year of graduation}
+#'     \item{first.name}{first name of graduate}
+#'     \item{last.name}{last name of graduate}
+#'     \item{gender}{based on \code{first.name}, using the gender package.}
+#'     \item{race}{based on \code{last.name}, using the wru package.}
+#'     \item{latin.honors}{character, Latin Honors recieved by graduate, if any}
+#'     \item{Phi.Beta.Kappa}{logical, indicates Phi Beta Kappa membership}
+#'     \item{Sigma.Xi}{logical, indicates Sigma Xi membership}
+#'     \item{honor}{first department honor awarded, if any. A handful
+#'       of graduates receive honors in two departments. We provide only the 
+#'       first. Values are "honors" or "highest honors"}
+#'     \item{major}{character, academic field in which graduate won primary honor,
+#'        if any}
 #' }
 #'
 "graduates"
